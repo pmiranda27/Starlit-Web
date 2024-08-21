317a40aa6d31eeca5509f851a519c30b41e2d789
PEDRO HENRIQUE MIRANDA DE VASCONCELOS <23143160@edu119.local>
2024-08-21 16:47:21 -0300

    First Commit

---
+++ src/Screens/Join_Us.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+const JoinUs = styled.main`
+    width: 100%;
+    height: 100vh;
+
+    background-image: url("../Assets/Images/background.png");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+`;
+
+const JoinUsPage = (
+    <JoinUs>
+        camisa 10
+    </JoinUs>
+);
+
+export default JoinUsPage;
