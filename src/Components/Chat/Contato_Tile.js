6bfefe007c647ccce8c1e190f0573133f4c508b8
pmiranda27 <pmiranda.senai29@gmail.com>
2024-11-22 16:40:48 -0300

    feat: estrutura da págian de chat.

---
+++ src/Components/Chat/Contato_Tile.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ContatoTileChat = styled.div`
+    width: 90%;
+    height: fit-content;
+
+    overflow: hidden;
+
+    padding: 8px 12px 8px 12px;
+
+    background-color: red;
+`;
--- src/Screens/Chats/Message.js
+++ src/Screens/Chats/Message.js
@@ -1,22 +1,51 @@
+import { ContatoTileChat } from "../../Components/Chat/Contato_Tile";
+import { useAmigos } from "../../Components/Services/Amigos_Service";
 import "./Message.css";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Chat() {
   const iconStyle = { color: "white" };
 
-  useEffect(() => {
-    let isMounted = true;
+  const { getListaAmigos } = useAmigos();
 
-    return () => {
-      isMounted = false;
-    };
+  const [listaObjetosAmigos, setListaObjetosAmigos] = useState();
+  const [listaAmigosContatos, setListaAmigosContatos] = useState([]);
+
+  async function getListaContatos() {
+    const response = await getListaAmigos();
+
+    setListaObjetosAmigos(response);
+  }
+
+  function componentGeneratorAmigos(lista) {
+    if(!lista){
+      return;
+    }
+    console.log('diglet', lista)
+    var novaListaComponents = lista.map((ami) => (
+      <ContatoTileChat>
+        <h3>{ami.name}</h3>
+      </ContatoTileChat>));
+
+    setListaAmigosContatos(novaListaComponents)
+  }
+
+  useEffect(() => {
+    getListaContatos();
   }, []);
+
+  useEffect(() => {
+    componentGeneratorAmigos(listaObjetosAmigos);
+  }, [listaObjetosAmigos])
 
   return (
     <>
       <div className="chat-main">
-        fasf
+        <section className="lista-de-contatos-chat">
+          {}
+        </section>
+        <section className="area-de-conversa-chat"></section>
       </div>
     </>
   );
